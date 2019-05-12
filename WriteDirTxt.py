98e5eeca5195c9bca7e639c77e48985d3fa61674
jiangguoqing <jiangguoqing@kuaishou.com>
2019-05-12 20:16:36 +0800

    读入wrf coawst数据

--- WriteDirTxt.py
+++ WriteDirTxt.py
@@ -6,18 +6,14 @@ import pandas as pd
 import numpy as np
 from datetime import datetime
 
-root_dir=r"/Users/ageliss/Downloads/Ocean_Predict_Center_data"
-# with open("./OutputAsTxt.txt", "w") as output:
-    # output.write("台风名称"+"\t"+"台风编号"+"\t"+"预测机构"+"\t"+"时间"+"\t"+"实况台风纬度"+"\t"+"实况台风经度"+"\t"+"台风最低气压"+"\t"
-    #              +"台风最大风速"+"\t"+"24lat"+"\t"+"24lon"+"\t"+"24pre"+"\t"+"24wind_seed"+"\t"+"48lat"+"\t"+"48lon"+"\t"+"48pre"+"\t"+
-    #              "48wind_seed"+"\t"+"72lat"+"\t"+"72lon"+"\t"+"72pre"+"\t"+"72wind_seed"+"\n")
+root_dir = r"/Users/ageliss/Downloads/Ocean_Predict_Center_data"
 list_dirs = os.walk(root_dir)
 res = pd.DataFrame(columns=['ty_name', 'ty_ID', 'agency', 'time', 'lon', 'lat', 'MSLP', 'MaxWind', '24lat', '24lon',
                             '24MSLP', '24MaxWind', '48lat', '48lon', '48MSLP', '48MaxWind',
                             '72lat', '72lon', '72MSLP', '72MaxWind'])
 for root, dirs, files in list_dirs:
     for d in dirs:
-        dir=os.path.join(root, d)
+        dir = os.path.join(root, d)
         for info in os.listdir(dir):
             if(info.endswith(".wrf") or info.endswith(".coawst") or info.endswith('.BAB')):
                 '''Write a seperate DataFrame for this kinds'''
@@ -26,7 +22,6 @@ for root, dirs, files in list_dirs:
             with open(file_name, 'r') as file:  # 读取文件内容
                 print('Reading: ', file_name)
                 line = file.readline()
-                # print(line.__str__())
                 eachline = line.split()
 
                 if eachline == []:
@@ -53,11 +48,6 @@ for root, dirs, files in list_dirs:
                     else:
                         eachline = lines.split()
 
-                        # output.write(ty_name + "\t" + ty_ID + "\t" + forecasting_agency + "\t" + eachline[0] + "-" +
-                        #              eachline[1] + "-" + eachline[2] + "时" + "\t")
-                        # for i in range(3,eachline.__len__()):
-                        #     output.write(eachline[i]+"\t")
-                        # print(ty_ID)
                         try:
                             time = datetime(int(year), int(eachline[0]), int(eachline[1]), int(eachline[2]))
                         except:
@@ -73,6 +63,6 @@ for root, dirs, files in list_dirs:
                                                     '48MSLP', '48MaxWind', '72lat', '72lon', '72MSLP', '72MaxWind'])
                         res = res.append(df1, ignore_index=True)
                         # output.write("\n")
-res.to_csv('Typhoon_data2.csv')
+res.to_csv('Typhoon_data3.csv')
 print(res)
 
